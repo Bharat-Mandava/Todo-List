5d062fe6342886e6425b5b43429488f70b57cc8f
Bharat-Mandava <43178562+Bharat-Mandava@users.noreply.github.com>
2019-04-25 19:22:41 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+let x = prompt('Please add Task 1','Clean my desk, etc.,');
+let y = prompt('what time you want to finish your task 1, 24 hours system', '14')
+let t = new Date()
+let timenow = t.getHours()
+document.getElementById('timenow').innerHTML =t;
+document.getElementById('task1').innerHTML =x;
+let z = y-timenow;
+document.getElementById('time1').innerHTML =z;
+//document.body.innerHTML = x; 
+
+//document.body.innerHTML = timenow;
+
+if (y == timenow) {
+alert('complete your task 1')
+let audio = new Audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
+    audio.play();
+}
+
+
+
